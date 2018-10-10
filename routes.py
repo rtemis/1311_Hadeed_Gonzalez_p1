12acab2e32fadb4361806b9f158e5f86f1209d1d
rtemis <leah_hadeed@yahoo.com>
2018-10-10 15:03:26 +0200

    intento 1

---
+++ routes.py
@@ -0,0 +1,12 @@
+import os
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
