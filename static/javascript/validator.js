e6e75cb4ab6d08e6c71a5d5ece913d5a98b90864
rtemis <leah_hadeed@yahoo.com>
2018-10-19 10:06:04 +0200

    empezando validacion

---
+++ static/javascript/validator.js
@@ -0,0 +1,28 @@
+function validate_login_usr() {
+
+}
+
+function validate_login_psw() {
+
+}
+
+function validate_usr() {
+
+}
+
+function validate_psw() {
+
+}
+
+function validate_email() {
+
+}
+
+function validate_name() {
+
+}
+
+function validate_creditcard() {
+	
+}
+
