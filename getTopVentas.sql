27d7f69139cdbbf2e1063a481762146e15d0840c
rtemis <leah_hadeed@yahoo.com>
2018-11-14 01:08:23 +0100

    hecho set price

--- getTopVentas.sql
+++ getTopVentas.sql
@@ -1,9 +1,45 @@
 ﻿drop function getTopVentas() cascade;
 
+drop view sales cascade ;
+drop view movies cascade ;
+drop view orderyears cascade ;
+drop view results cascade ;
 create or replace function getTopVentas(integer anno) returns setof record as $$
 	declare 
+		maximum bigint := 0;
 		temp1 record;
+		temp2 record;
 	begin 
-		for select * in 
+		create view sales as 
+			select prod_id, count(quantity) as ventas
+			from orderdetail
+			group by prod_id;
+			
+		create view movies as
+			select prod_id, movietitle
+			from products natural join imdb_movies
+			where products.movieid=imdb_movies.movieid;
+
+		create view moviesales as 
+			select * 
+			from movies natural join sales;
+
+		create view orderyears as 
+			select prod_id, date_part('year',orderdate) as yr
+			from orders natural join orderdetail
+			order by yr desc;
+
+		create view results as 
+			select yr, movietitle, max(ventas)
+			from moviesales natural join orderyears
+			group by yr, movietitle, ventas
+			order by yr desc;
+
+		select * from results;
+		
+		
+		
 	end;
 $$ language 'plpgsql';
+
+select getTopVentas(1970);
--- setOrderAmount.sql
+++ setOrderAmount.sql
@@ -1,27 +1,36 @@
-﻿
+﻿drop view totals;
+drop view netamt;
 drop function setOrderAmount();
 
 create or replace function setOrderAmount() returns void as $$
 	begin
+		CREATE VIEW netamt AS
+			SELECT orderid, SUM(price * quantity) AS total
+			FROM orderdetail
+			GROUP BY orderid;
+			
 		UPDATE orders
 		SET netamount = t2.total
 		FROM (
-			SELECT orderid, SUM(price * quantity) AS total
-			FROM orderdetail
-			GROUP BY orderid
-		) AS t2 where orders.orderid = t2.orderid;
+			SELECT orderid, round(avg(total)::NUMERIC,2) as total
+			FROM netamt
+			GROUP BY netamt.orderid
+		) AS t2 WHERE orders.orderid = t2.orderid;
 
-		UPDATE orders 
-		SET totalamount = t2.total
+		CREATE VIEW totals AS 
+			select orderid, sum(netamount + (netamount * (tax/100.0))) as newtotal
+			from orders 
+			group by orderid;
+
+		UPDATE orders
+		SET totalamount = t2.newtotal 
 		FROM (
-			SELECT orderid, sum(netamount + (netamount * (tax/100.0))) AS total
-			FROM orders
-			GROUP BY orderid
-		) AS t2 where orders.orderid = t2.orderid; 
-		
+			SELECT orderid, round(avg(newtotal)::NUMERIC,2) as newtotal
+			FROM totals
+			GROUP BY totals.orderid
+		) AS t2 WHERE orders.orderid = t2.orderid;
 	end;
 $$ language 'plpgsql';
 
 select setOrderAmount();
-
 
