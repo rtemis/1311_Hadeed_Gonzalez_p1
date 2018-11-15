5cae58d1281f23750576165e81df877c4acc4a4f
Tu Nombre <you@example.com>
2018-11-15 14:34:33 +0100

    setPrice corregido

--- setPrice.sql
+++ setPrice.sql
@@ -1,4 +1,4 @@
 ﻿update orderdetail
-set price = products.price - (products.price * (0.02 * (date_part('year', CURRENT_DATE) - date_part('year', orders.orderdate)))) 
+set price = (products.price * (0.98 ^ (date_part('year', CURRENT_DATE) - date_part('year', orders.orderdate)))) 
 from products, orders
 where orderdetail.prod_id = products.prod_id and orders.orderid = orderdetail.orderid;
