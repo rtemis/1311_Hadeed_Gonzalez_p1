3a04cfb6c7649343c0ffd4b745ea61b16aaf375e
Tu Nombre <you@example.com>
2018-11-21 18:45:03 +0100

    intentando trigger

--- updOrders.sql
+++ updOrders.sql
@@ -4,9 +4,9 @@
 	-- If the trigger was caused by insert
 		IF (TG_OP = 'INSERT') THEN
 		-- Updating net amount
-			
-			SELECT ROUND(price::NUMERIC,2) INTO NEW.price
-			from products where NEW.prod_id=prod_id;
+
+			NEW.price = (SELECT ROUND(price::NUMERIC,2)
+			FROM products WHERE NEW.prod_id=prod_id);
 
 			UPDATE orders
 			SET netamount = ROUND((netamount + (NEW.price * NEW.quantity))::NUMERIC,2)
@@ -27,16 +27,36 @@
 			UPDATE orders
 			SET totalamount = ROUND((netamount * (1 + tax/100.0))::NUMERIC,2)
 			WHERE OLD.orderid = orders.orderid;	
-			RETURN OLD;		
+			RETURN OLD;
+
+		ELSIF (TG_OP = 'UPDATE') THEN
+		
+			UPDATE orders
+			SET netamount = ROUND((OLD.price * NEW.quantity)::NUMERIC,2)
+			WHERE OLD.orderid = orders.orderid;
+
+			UPDATE orders
+			SET totalamount = ROUND((netamount * (1 + tax/100.0))::NUMERIC,2)
+			WHERE OLD.orderid = orders.orderid;	
+			RETURN OLD;
 		END IF; 
 			
 	END;
 $$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER updOrders BEFORE INSERT OR DELETE on orderdetail
+--DROP TRIGGER updOrders on orderdetail
+CREATE TRIGGER updOrders BEFORE INSERT OR DELETE OR UPDATE on orderdetail
 	FOR EACH ROW EXECUTE PROCEDURE f_updOrders();
 
---insert into orderdetail (orderid, prod_id, quantity) values (140320,1,9);
---select * from orderdetail natural join orders where orderid = 140320;
---delete from orderdetail where orderid=140320 and prod_id=1;
---select * from orderdetail natural join orders where orderid = 140320;
+insert into orders (orderid, orderdate, customerid, status) values (181791, current_date, 30, NULL);
+
+insert into orderdetail (orderid, prod_id, quantity) values (181791,1,9);
+select * from orderdetail natural join orders where orderid = 181791;
+
+update orderdetail set quantity=quantity-1 where prod_id=1 and orderid=181791;
+select * from orderdetail natural join orders where orderid = 181791;
+
+delete from orders where orderid=181791;
+
+delete from orderdetail where orderid=181791;
+select * from orderdetail natural join orders where orderid = 181791;
