735e7a010c68e68af6296eea0739905429addacf
rtemis <leah_hadeed@yahoo.com>
2018-11-22 21:29:36 +0100

    No se si funciona ahora pero lo entrego y manana preguntamos

--- updInventory.sql
+++ updInventory.sql
@@ -16,36 +16,36 @@
 			) AS T1
 			WHERE inventory.prod_id = T1.prod_id;
 
-			FOR temp1 IN (SELECT * FROM inventory) LOOP
-				IF (temp1.stock < 1) THEN
-					INSERT INTO alerts (prod_id, stock) VALUES (temp1.prod_id, temp1.stock);
-				END IF;
-			END LOOP;
+		--	FOR temp1 IN (SELECT * FROM inventory) LOOP
+			--	IF (temp1.stock < 1) THEN
+				--	INSERT INTO alerts (prod_id, stock) VALUES (temp1.prod_id, temp1.stock);
+		--		END IF;
+			--END LOOP;
 		END IF;
 
 		RETURN OLD;
 	END;
 $$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER updInventory BEFORE UPDATE ON orders 
+CREATE TRIGGER updInventory AFTER UPDATE ON orders 
 	FOR EACH ROW EXECUTE PROCEDURE f_updInventory();
 
 
-insert into orders (orderid, orderdate, customerid) values (181791, current_date, 30);
-insert into orderdetail (orderid, prod_id, quantity) values (181791,7,9);
-select * from orderdetail natural join orders where orderid = 181791;
+--insert into orders (orderid, orderdate, customerid) values (181791, current_date, 30);
+--insert into orderdetail (orderid, prod_id, quantity) values (181791,7,9);
+--select * from orderdetail natural join orders where orderid = 181791;
 
-select * from alerts
+--select * from alerts
 
-insert into orderdetail (orderid, prod_id, quantity) values (181791,6,9);
-select * from orderdetail natural join orders where orderid = 181791;
+--insert into orderdetail (orderid, prod_id, quantity) values (181791,6,9);
+--select * from orderdetail natural join orders where orderid = 181791;
 
-update orderdetail set quantity=quantity-8 where prod_id=6 and orderid=181791;
-select * from orderdetail natural join orders where orderid = 181791;
+--update orderdetail set quantity=quantity-8 where prod_id=6 and orderid=181791;
+--select * from orderdetail natural join orders where orderid = 181791;
 
-delete from orderdetail where orderid=181791;
-select * from orderdetail natural join orders where orderid = 181791;
-delete from orders where orderid=181791;
+--delete from orderdetail where orderid=181791;
+--select * from orderdetail natural join orders where orderid = 181791;
+--delete from orders where orderid=181791;
 
 
-update orders set status = 'Paid' where orderid = 181791;
+--update orders set status = 'Paid' where orderid = 181791;
