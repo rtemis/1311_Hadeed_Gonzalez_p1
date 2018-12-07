7ac6f1221a1a9a72c0fea3035251a7b45eb68d84
Tu Nombre <you@example.com>
2018-12-07 11:37:03 +0100

    casi acabado optimizacion

---
+++ P4/app/countStatus.sql
@@ -0,0 +1,17 @@
+﻿CREATE INDEX IF NOT EXISTS estado ON orders(status); 
+
+EXPLAIN select count(*) 
+from orders 
+where status is null;
+
+EXPLAIN select count(*) 
+from orders 
+where status ='Shipped';
+
+select count(*) 
+from orders 
+where status ='Paid';
+
+select count(*) 
+from orders 
+where status ='Processed';
