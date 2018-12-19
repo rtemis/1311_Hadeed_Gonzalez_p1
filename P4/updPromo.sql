2a6b5dff91bfc8a9b899e00004315fd3c05a65f0
Your Name <you@example.com>
2018-12-20 00:21:50 +0100

    x

---
+++ P4/updPromo.sql
@@ -0,0 +1,12 @@
+﻿alter table customers add column promo numeric(7,2);
+
+create or replace function f_updPromo() returns trigger as $$
+	begin
+		update orders
+		set totalamount = orderamount * (100-NEW.promo);
+	end;
+$$ language 'plpgsql';
+
+
+create trigger updPromo before update on customers
+    FOR EACH ROW EXECUTE PROCEDURE f_updPromo();
